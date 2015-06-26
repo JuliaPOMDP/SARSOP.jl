f99acf713c5a0d59eb4390d9229c7b41337b8fae
Tim Wheeler <wheelert@stanford.edu>
2015-06-25 23:20:29 -0700

    initial commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using SARSOP
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+policy = PolicyFile("mypolicy.policy")
+pomdp = POMDPFile("/home/tim/Documents/wheelerworkspace/SARSOP/appl-0.96/examples/POMDPX/Tiger.pomdpx")
+solver = SARSOPSolver(fast=true)
+solve!(policy, solver, pomdp)
+
+simulator = SARSOPSimulator(5, 5)
+simulate(simulator, policy, pomdp)
+
+evaluator = SARSOPEvaluator(5, 5)
+evaluate(evaluator, policy, pomdp)
+
+graphgen = PolicyGraphGenerator("Tiger.dot")
+polgraph(graphgen, policy, pomdp)
+
+to_pomdpx(POMDPFile("/home/tim/Documents/wheelerworkspace/SARSOP/appl-0.96/examples/POMDP/Tiger.pomdp"))
