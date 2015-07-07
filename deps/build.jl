23d9e333e8a938cbab046a362431a0b74a16b519
rtrochet <rtrochet@gmail.com>
2015-07-07 15:44:07 -0700

    For OS X only, edit files so 'make' will run

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-@osx ? error("Mac OS is not supported by the original SARSOP software.") : (@windows ? error("SARSOP.jl does not support Windows at this time.") : false)
+@windows_only error("SARSOP.jl does not support Windows at this time.")
 
 if ispath("appl-0.96")
     rm("appl-0.96", recursive=true)
@@ -13,5 +13,31 @@ if isfile("appl-0.96.tar")
     rm("appl-0.96.tar")
 end
 
+
 cd("appl-0.96/src")
+
+function replaceLine(filename::String, linenum::Int, textofline::String)
+    outfile = open("temp.txt", "w")
+    infile = open(filename)
+    i = 1
+    for line in eachline(infile)
+        if i == linenum
+            write(outfile, textofline)
+            i += 1
+        else
+            write(outfile, line)
+            i += 1
+        end
+    end
+    close(outfile)
+    close(infile)
+    mv("temp.txt", filename)
+end
+
+@osx_only begin
+    cd("MathLib")
+    replaceLine("SparseMatrix.h", 24, "            inline SparseCol() {}\n")
+    replaceLine("SparseVector.cpp", 491, "        //printf(\"Iter: %X\\n\", iter);\n")
+    cd("../")
+end
 run(`make`)
