81bbd0ec76e5a16edf2ccb12df90d08785cdeb25
Mykel Kochenderfer <mykel@stanford.edu>
2015-11-18 19:58:34 -0800

    add windows support

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,3 @@
-@windows_only error("SARSOP.jl does not support Windows at this time.")
-
 if !isdir(Pkg.dir("POMDPXFile"))
     Pkg.clone("https://github.com/sisl/POMDPXFile.jl")
 end
@@ -8,41 +6,49 @@ if ispath("appl-0.96")
     rm("appl-0.96", recursive=true)
 end
 
-download("http://bigbird.comp.nus.edu.sg/pmwiki/farm/appl/uploads/Main/appl-0.96.tar.gz", "appl-0.96.tar.gz")
-run(`gunzip appl-0.96.tar.gz`)
-run(`tar -xvf appl-0.96.tar`)
-rm("appl-0.96.tar")
+@windows_only begin
+    run(`mkdir appl-0.96`)
+    cd("appl-0.96")
+    run(`mkdir src`)
+    cd("src")
+    download("http://web.stanford.edu/group/sisl/resources/appl-0.96-win-x64.zip", "appl-0.96-win-x64.zip")
+    run(`unzip appl-0.96-win-x64.zip`)
+end
 
-if isfile("appl-0.96.tar")
+@unix_only begin
+    download("http://bigbird.comp.nus.edu.sg/pmwiki/farm/appl/uploads/Main/appl-0.96.tar.gz", "appl-0.96.tar.gz")
+    run(`gunzip appl-0.96.tar.gz`)
+    run(`tar -xvf appl-0.96.tar`)
     rm("appl-0.96.tar")
-end
-
-
-cd("appl-0.96/src")
-
-function replaceLine(filename::String, linenum::Int, textofline::String)
-    outfile = open("temp.txt", "w")
-    infile = open(filename)
-    i = 1
-    for line in eachline(infile)
-        if i == linenum
-            write(outfile, textofline)
-            i += 1
-        else
-            write(outfile, line)
-            i += 1
-        end
+    if isfile("appl-0.96.tar")
+        rm("appl-0.96.tar")
     end
-    close(outfile)
-    close(infile)
-    rm(filename)
-    mv("temp.txt", filename)
-end
+    cd("appl-0.96/src")
 
-@osx_only begin
-    cd("MathLib")
-    replaceLine("SparseMatrix.h", 24, "            inline SparseCol() {}\n")
-    replaceLine("SparseVector.cpp", 491, "        //printf(\"Iter: %X\\n\", iter);\n")
-    cd("../")
+    function replaceLine(filename::String, linenum::Int, textofline::String)
+        outfile = open("temp.txt", "w")
+        infile = open(filename)
+        i = 1
+        for line in eachline(infile)
+            if i == linenum
+                write(outfile, textofline)
+                i += 1
+            else
+                write(outfile, line)
+                i += 1
+            end
+        end
+        close(outfile)
+        close(infile)
+        rm(filename)
+        mv("temp.txt", filename)
+    end
+
+    @osx_only begin
+        cd("MathLib")
+        replaceLine("SparseMatrix.h", 24, "            inline SparseCol() {}\n")
+        replaceLine("SparseVector.cpp", 491, "        //printf(\"Iter: %X\\n\", iter);\n")
+        cd("../")
+    end
+    run(`make`)
 end
-run(`make`)
